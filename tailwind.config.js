6a5d905fea6bbd8511993bfeef2f9ad97b5b7e4e
ArnoldoRicardo <anyback@gmail.com>
2022-09-12 13:46:14 -0500

    update talwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{ts,tsx}',
     './ui/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
       serif:
         '"Cormorant Garamond", ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
